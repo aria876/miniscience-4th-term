b76f941bcb3f936e3635da120fb8e07c7b0a0dd6
Aria <sanzheev.av@phystech.edu>
2026-02-07 08:47:32 +0300

    first commit

---
+++ build/CMakeFiles/sample05.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample05.dir/sample05.cpp.o"
+  "CMakeFiles/sample05.dir/sample05.cpp.o.d"
+  "sample05"
+  "sample05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sample06.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample06.dir/sample06.cpp.o"
+  "CMakeFiles/sample06.dir/sample06.cpp.o.d"
+  "sample06"
+  "sample06.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
